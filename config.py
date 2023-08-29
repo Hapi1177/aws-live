f14dd5d0cdd54a8defe7fae8dc62aab7f39cdc81
Hapi1177 <143473497+Hapi1177@users.noreply.github.com>
2023-08-29 11:42:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cunul02ehqgv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "stevenleong-employee"
+customregion = "us-east-1"
+
