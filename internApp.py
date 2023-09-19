4de2d544b53cf057fb183b586be22c809b7c8cfb
Hapi1177 <143473497+Hapi1177@users.noreply.github.com>
2023-09-19 21:38:34 +0800

    Update internApp.py

--- internApp.py
+++ internApp.py
@@ -25,9 +25,9 @@ table = 'company'
 def index():
     return render_template('index.html')
 
-@app.route("/signup")
+@app.route("/signup", methods=['POST'])
+def company_signup():
     return render_template('signup.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
